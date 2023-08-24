f7c7e8d593bc3c6c56d084a70e6f4f0fa1f2b248
-MGriselda- <46288252+googlerax@users.noreply.github.com>
2023-08-24 20:36:47 -0300

    first commit- comienzo de conversor de moneda

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import conversion.ToCurrency;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        ToCurrency test = new ToCurrency();
+
+        test.pesoToDolar(1000);
+        test.pesoToEuro(1000);
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/conversion/ToCurrency.java
@@ -0,0 +1,39 @@
+package conversion;
+
+import javax.swing.*;
+
+public class ToCurrency {
+
+
+//    Metodo de peso Argentino a Dolares
+    public void pesoToDolar(double val){
+        double dolar = val*0.0029;
+        System.out.println(dolar);
+        JOptionPane.showMessageDialog(null,"$"+val+" ARS equivalen a "+dolar+ " USD", "Peso a Dolar Americano", JOptionPane.PLAIN_MESSAGE);
+    }
+//"Título del Cuadro de Diálogo"
+    public void pesoToEuro(double val){
+        double euro = val * 0.0026;
+        JOptionPane.showMessageDialog(null,"$"+val+" ARS equivalen a "+euro+ " EUR", "Peso a Euros", JOptionPane.PLAIN_MESSAGE);
+    }
+
+    public void pesoToLibra(double val){
+        double libra = val * 0.0023;
+        JOptionPane.showMessageDialog(null,"$"+val+" ARS equivalen a "+libra+ " GBP", "Peso a Libra Esterlina", JOptionPane.PLAIN_MESSAGE);
+    }
+
+    public void pesoToYen(double val){
+        double yen = val * 0.42;
+        JOptionPane.showMessageDialog(null,"$"+val+" ARS equivalen a "+yen+ " JPY", "Peso a Yen Japones", JOptionPane.PLAIN_MESSAGE);
+    }
+
+    public void pesoToWon(double val){
+        double won = val*3.81;
+        JOptionPane.showMessageDialog(null,"$"+val+" ARS equivalen a "+won+ " USD", "Peso a KWR", JOptionPane.PLAIN_MESSAGE);
+    }
+
+
+
+
+
+}
