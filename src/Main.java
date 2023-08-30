3bf9c0231e3c5a50eb6928ef91b0ff415882bb4d
-MGriselda- <46288252+googlerax@users.noreply.github.com>
2023-08-30 00:42:11 -0300

    test de incorporacion de iconos con javax.swing.ImageIcon

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,19 @@ public class Main {
     public static void main(String[] args) {
 
 
-        FunOptions money = new FunOptions();
+//        FunOptions money = new FunOptions();
+//
+//
+//        money.conversionMonedas(1);
 
 
-        money.conversionMonedas(1);
+//        test de incorporacion de iconos con javax.swing.ImageIcon
 
+        ImageIcon customIcon = new ImageIcon("D:\\PROGRAMACION_\\ONE-oracle\\BACK-END-JAVA\\currency-converter\\src\\iconDollar.png");
+
+        // Mostrar un cuadro de diálogo con el icono personalizado
+        JOptionPane.showMessageDialog(null, "Esto es un mensaje con un icono personalizado.", "Icono Personalizado", JOptionPane.PLAIN_MESSAGE, customIcon);
+    }
 
 
     }
-}
